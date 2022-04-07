3ec6a4e6b526171582d989a873f4c56107e786d1
future <twixxx2457@gmail.com>
2022-04-07 15:35:26 +0300

    master 3

--- new_web/settings.py
+++ new_web/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'new_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'new_pr',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'Alex101200',
+        'PORT': '1342'
     }
 }
 
