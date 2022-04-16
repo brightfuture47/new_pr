542d7020fdbe0083c8a07b44eadbf390eb0f87aa
future <twixxx2457@gmail.com>
2022-04-16 20:28:13 +0300

    Pr 14 final

--- web_lib/urls.py
+++ web_lib/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from web_lib.views import main
+from web_lib.views import main, authors, books
 
 urlpatterns= [
     path('', main, name='web_lib'),
+    path('authors', authors, name='authors'),
+    path('books', books, name='books'),
 
 ]
--- web_lib/views.py
+++ web_lib/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from web_lib.models import Author, Book
 
 def main(request):
     return render(request, 'web_lib/main.html')
+
+def authors(request):
+    all_authors = {'authors': Author.objects.all()}
+    return render(request, 'web_lib/authors.html', all_authors )
+
+def books(request):
+    all_books = {'books': Book.objects.all()}
+    return render(request, 'web_lib/books.html', all_books)
