8b69ec26e3ab131a3a5a1e2112e55561bc4c119b
future <twixxx2457@gmail.com>
2022-04-21 10:35:00 +0300

    final 16 pr

--- web_lib/urls.py
+++ web_lib/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from web_lib.views import main, authors, books, about
+from web_lib.views import main, authors, books, about, author_id
 
 urlpatterns= [
     path('', main, name='web_lib'),
     path('authors', authors, name='authors'),
+    path('author/<uuid:pk>', author_id, name='author_id'),
     path('books', books, name='books'),
     path('about', about, name='about'),
 
--- web_lib/views.py
+++ web_lib/views.py
@@ -8,6 +8,12 @@ def authors(request):
     all_authors = {'authors': Author.objects.all()}
     return render(request, 'web_lib/authors.html', all_authors )
 
+def author_id(request, pk):
+    author = Author.objects.get(pk=pk)
+    books_amount = author.book_set.count()
+    found_author = {'author': author, 'books_amount': books_amount}
+    return render(request, 'web_lib/author_id.html', found_author)
+
 def books(request):
     all_books = {'books': Book.objects.all()}
     return render(request, 'web_lib/books.html', all_books)
