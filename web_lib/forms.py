3646c6dd8773bbcef155892a9bae30fe2bd65a60
future <twixxx2457@gmail.com>
2022-05-17 17:52:56 +0300

    pr 18 final

--- web_lib/forms.py
+++ web_lib/forms.py
@@ -1,5 +1,9 @@
-from tkinter.tix import Form
-from django.forms import forms
+from django import forms
 
-class SearchAuthor(forms,Form):
-    uuid = forms.UUIDField(label="Author UUID")
+class SearchAuthor(forms.Form):
+    author_uuid = forms.UUIDField(label="Author UUID", required= False)
+
+class PostAuthors(forms.Form):
+    name = forms.CharField(label ="Name", max_length=150, required= False)
+    age = forms.IntegerField(label="Age", required= False)
+    email = forms.EmailField(label="Email", required= False)
--- web_lib/views.py
+++ web_lib/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from web_lib.models import Author, Book
 
+from.forms import SearchAuthor, PostAuthors
+
 def main(request):
-    return render(request, 'web_lib/main.html')
+    if request.method == "GET":
+        form = SearchAuthor(request.GET)
+        form_post = PostAuthors(request.POST)
+        return render(request, 'web_lib/main.html',{"form": form, "form_post": form_post})
+"""     return render(request, 'web_lib/main.html') """
+
+""" def form_search(req):
+    if req.method == "GET":
+        form = SearchAuthor(req.GET)
+        return render(req, 'web_lib/main.html',{"form": form}) """
 
 def authors(request):
+    if "author_uuid" in request.GET:
+        return redirect('author_id', request.GET["author_uuid"])
+    if request.method == "POST":
+        data = dict()
+        data["name"] = request.POST.get('name')
+        data["age"] = request.POST.get('age')
+        data["email"] = request.POST.get('email')
+        Author.objects.create(**data)
+
     all_authors = {'authors': Author.objects.all()}
     return render(request, 'web_lib/authors.html', all_authors )
 
